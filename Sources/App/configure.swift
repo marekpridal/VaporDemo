8297eccfe735f15b25c96bf42040b425eb4c584d
Marek Pridal <marek@marekpridal.eu>
2019-01-22 18:18:04 +0100

    Change port on endpoint

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,4 +7,8 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
+    
+    // Use port 8001
+    let myService = NIOServerConfig.default(port: 8001)
+    services.register(myService)
 }
