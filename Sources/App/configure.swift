4bbb0dc6ac71d4e88e7c235bb8a912effbbea7de
Marek Přidal <marek@marekpridal.eu>
2019-01-25 10:57:00 +0100

    Connect to JAWS DB MySQL server

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -15,12 +15,22 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     try services.register(FluentMySQLProvider())
     
     var databases = DatabasesConfig()
-    let databaseConfig = MySQLDatabaseConfig(hostname: "127.0.0.1",
-                                             port: 3306,
-                                             username: "root", 
-                                             password: "root",
-                                             database: "vydaje",
-                                             transport: MySQLTransportConfig.unverifiedTLS)
+    var databaseConfig: MySQLDatabaseConfig
+    if env.isRelease {
+        databaseConfig = MySQLDatabaseConfig(hostname: Environment.get("JAWSDB_URL")!,
+                                             port: Int(Environment.get("JAWSDB_PORT")!)!,
+                                             username: Environment.get("JAWSDB_USERNAME")!,
+                                             password: Environment.get("JAWSDB_PASSWORD")!,
+                                             database: Environment.get("JAWSDB_DATABASE")!)
+    } else {
+        databaseConfig = MySQLDatabaseConfig(hostname: "127.0.0.1",
+                                                 port: 3306,
+                                                 username: "root",
+                                                 password: "root",
+                                                 database: "vydaje",
+                                                 transport: MySQLTransportConfig.unverifiedTLS)
+
+    }
     let database = MySQLDatabase(config: databaseConfig)
     databases.add(database: database, as: .mysql)
     services.register(databases)
